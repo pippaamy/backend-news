1f151a7b5cb5a1a624bc59666e6a6e091af02a2d
Pippa Austin <pippaaustin@MacBook-Pro.local>
2023-10-09 10:27:44 +0100

    added api endpoint and testing

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { getTopics } = require("./controller/controller");
+const { getTopics, getApi } = require("./controller/controller");
 const app = express();
 
 app.get("/api/topics", getTopics);
+
+app.get("/api", getApi);
 
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "Path not found" });
