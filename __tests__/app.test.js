87a7f426590a403ccf6b97a91af0288218969629
Pippa Austin <pippaaustin@MacBook-Pro.local>
2023-10-09 11:17:49 +0100

    task 4 testing and /api/articles/:article_id endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -40,13 +40,53 @@ describe("Error handling for non-existant paths", () => {
   });
 });
 
-describe("get endpoints", () => {
+describe("GET endpoints", () => {
   test("should return a 200 and the list of endpoints", () => {
     return request(app)
       .get("/api")
       .expect(200)
       .then(({ body }) => {
         expect(body.endpoints).toEqual(endpointsJSON);
+      });
+  });
+});
+
+describe("GET api/articles/:article_id", () => {
+  test("should return a 200 and an article list with all the correct properties ", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        body.article.forEach((article) => {
+          expect(article).toEqual(
+            expect.objectContaining({
+              author: expect.any(String),
+              title: expect.any(String),
+              article_id: 1,
+              body: expect.any(String),
+              topic: expect.any(String),
+              created_at: expect.any(String),
+              votes: expect.any(Number),
+              article_img_url: expect.any(String),
+            })
+          );
+        });
+      });
+  });
+  test("should return a 400 if invalid id used", () => {
+    return request(app)
+      .get("/api/articles/turtle")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad Request");
+      });
+  });
+  test("should return a 404 if non-existant id used", () => {
+    return request(app)
+      .get("/api/articles/9090909")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Article not found");
       });
   });
 });
--- model/model.js
+++ model/model.js
@@ -5,3 +5,18 @@ exports.selectTopics = () => {
     return rows;
   });
 };
+
+exports.selectArticleById = (article_id) => {
+  return db
+    .query("SELECT * FROM articles WHERE article_id =$1;", [article_id])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: "Article not found",
+        });
+      } else {
+        return rows;
+      }
+    });
+};
