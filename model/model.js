fc7a905f69e99acd415db833b33eaf059c0352ad
Pippa Austin <pippaaustin@MacBook-Pro.local>
2023-10-23 11:59:28 +0100

    get user by username

--- model/model.js
+++ model/model.js
@@ -113,3 +113,17 @@ exports.fetchUsers = () => {
     return rows;
   });
 };
+
+exports.fetchUserByUsername = (username) => {
+  return db
+    .query("SELECT * FROM users WHERE users.username = $1;", [username])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: "Path not found",
+        });
+      }
+      return rows[0];
+    });
+};
