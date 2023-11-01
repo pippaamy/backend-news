5c2b27a6d4892b1203df25298d26b1bd1618aed0
Pippa Austin <pippaaustin@MacBook-Pro.local>
2023-11-01 12:11:29 +0000

    task22 post topic with error testing

--- model/model.js
+++ model/model.js
@@ -201,3 +201,20 @@ exports.createArticle = (author, title, body, topic, article_img_url) => {
       return rows[0];
     });
 };
+
+exports.addTopic = (slug, description) => {
+  if (!slug || !description) {
+    return Promise.reject({
+      status: 400,
+      msg: "Bad Request",
+    });
+  }
+  return db
+    .query(
+      "INSERT INTO topics ( slug, description) VALUES ( $1,$2) RETURNING *;",
+      [slug, description]
+    )
+    .then(({ rows }) => {
+      return rows[0];
+    });
+};
